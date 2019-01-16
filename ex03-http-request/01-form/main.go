8d6b55465159d8f9cbf8cf246cfbb4f0c3e1ce95
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2019-01-17 00:50:59 +0200

    HTTP Request with form params ex.

---
+++ ex03-http-request/01-form/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type anything int
+
+func (m anything) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("we are in serveHTTP")
+	fmt.Println(req)
+	tpl.ExecuteTemplate(w, "index.gohtml", req.PostForm)
+}
+
+var tpl *template.Template
+
+func init() {
+	fmt.Println("now we are on init")
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
+	var d anything
+	fmt.Println("now we are on main")
+	http.ListenAndServe(":8080", d)
+}
